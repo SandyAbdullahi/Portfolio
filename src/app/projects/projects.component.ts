180a919c8f62c98c3d81c149fe231851d1484290
AbdullahinurAA <abdullahinur.abdullahi@gmail.com>
2020-06-30 18:29:24 +0300

    complete config for project parallax

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IParallaxScrollConfig } from 'ngx-parallax-scroll'
 
 @Component({
   selector: 'app-projects',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
+
+  projectConfig: IParallaxScrollConfig = {
+    parallaxSpeed: 0.6,
+    parallaxSmoothness: 0.1,
+    parallaxDirection: 'reverse',
+    parallaxTimingFunction: 'linear',
+    parallaxThrottleTime: 8
+  };
 
   constructor() { }
 
