b6df6529e741fdc8b70794f6a23953e6e8a94f97
AbdullahinurAA <abdullahinur.abdullahi@gmail.com>
2020-06-30 12:13:01 +0300

    rewverting commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Import font awesome
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+
+
 
 @NgModule({
   declarations: [
@@ -12,7 +17,8 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
