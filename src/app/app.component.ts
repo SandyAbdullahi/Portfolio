f14fd4f5449265e67f5c3b0bf82ec9adcaee06c8
AbdullahinurAA <abdullahinur.abdullahi@gmail.com>
2020-07-01 10:41:39 +0300

    fix reponsive issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faFish } from "@fortawesome/free-solid-svg-icons";
+import { IParallaxScrollConfig } from 'ngx-parallax-scroll'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   faFish = faFish;
+
+  WorkStatusConfig: IParallaxScrollConfig = {
+    parallaxSpeed: 0.2,
+    parallaxSmoothness: 0.1,
+    parallaxDirection: 'reverse',
+    parallaxTimingFunction: 'ease',
+    parallaxThrottleTime: 1
+  };
+
+
+  ngOnInit(): void {
+
+  }
 }
