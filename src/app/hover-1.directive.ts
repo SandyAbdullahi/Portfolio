35938abe5c5cf509132c2c2e17dc49d2707b03cf
AbdullahinurAA <abdullahinur.abdullahi@gmail.com>
2020-07-05 11:10:33 +0300

    add button styling for projects and change margins

--- src/app/hover-1.directive.ts
+++ src/app/hover-1.directive.ts
@@ -1,16 +1,28 @@
 import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appHover1]'
+  selector: '[Hover1]'
 })
 export class Hover1Directive {
 
   constructor(public el: ElementRef) { }
 
   @HostListener('mouseenter') onMouseEnter() {
+    var topHeader = document.querySelector('.top-header');
+    topHeader.classList.add('top-header-blue');
+    var topHeader = document.querySelector('.inner-header');
+    topHeader.classList.add('inner-header-blue');
+    var topHeader = document.querySelector('#show');
+    topHeader.classList.remove('hide');
 
   };
-  @HostListener('mouseenter') onMouseLeave() {
+  @HostListener('mouseleave') onMouseLeave() {
+    var topHeader = document.querySelector('.top-header');
+    topHeader.classList.remove('top-header-blue');
+    var topHeader = document.querySelector('.inner-header');
+    topHeader.classList.remove('inner-header-blue');
+    var topHeader = document.querySelector('#show');
+    topHeader.classList.add('hide');
 
   };
 
